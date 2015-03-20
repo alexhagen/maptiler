e55f53b45d9b201733001df5f2389bfd3c0146cd
Alex Hagen <ahagen@purdue.edu>
2015-03-19 22:12:39 -0400

    working analysis code creating an svg file, needs size change and compilation into full svg

---
+++ wave_process.py
@@ -0,0 +1,55 @@
+import wave
+import numpy as np
+import matplotlib
+matplotlib.use('svg')
+import matplotlib.pyplot as plt
+
+# Created input file with:
+# mpg123  -w 20130509talk.wav 20130509talk.mp3
+wr = wave.open('riversandroads.wav', 'r')
+par = list(wr.getparams()) # Get the parameters from the input.
+# This file is stereo, 2 bytes/sample, 44.1 kHz.
+par[3] = 0 # The number of samples will be set by writeframes.
+
+# Open the output file
+#ww = wave.open('riversandroads-filtered.wav', 'w')
+#ww.setparams(tuple(par)) # Use the same parameters as the input file.
+
+lowpass = 21 # Remove lower frequencies.
+highpass = 9000 # Remove higher frequencies.
+
+sz = wr.getframerate() # Read and process 1 second at a time.
+c = int(wr.getnframes()/sz) # whole file
+trackl = [];
+trackr = [];
+for num in range(c):
+	print('Processing {}/{} s'.format(num+1, c))
+	da = np.fromstring(wr.readframes(sz), dtype=np.int16)
+	left, right = da[0::2], da[1::2] # left and right channel
+	#lf, rf = np.fft.rfft(left), np.fft.rfft(right)
+	#lf[:lowpass], rf[:lowpass] = 0, 0 # low pass filter
+	#lf[55:66], rf[55:66] = 0, 0 # line noise
+	#lf[highpass:], rf[highpass:] = 0,0 # high pass filter
+	#nl, nr = np.fft.irfft(lf), np.fft.irfft(rf)
+	#ns = np.column_stack((nl,nr)).ravel().astype(np.int16)
+	#ww.writeframes(ns.tostring())
+	trackl.extend(left)
+	trackr.extend(right)
+sections = 10
+sect_size = (np.size(trackl)-1)/sections
+for section in range(0,sections):
+	frames = section * sect_size + np.arange(sect_size)
+	l = trackl[np.min(frames):np.max(frames)+1]
+	r = trackr[np.min(frames):np.max(frames)+1]
+	avg = np.divide(np.add(l,r),2.0);
+	fig = plt.figure(num=section+1,figsize=(8, 6), dpi=300)
+	a = plt.axes([0, 0, 1, 1])
+	plt.plot(frames,avg,'k-')
+	plt.axis('off')
+	maximum = np.max([np.max(trackl),np.max(trackr)]);
+	plt.ylim(-maximum,maximum);
+	plt.xlim(np.min(frames),np.max(frames));
+	plt.savefig('line_%d.svg' % (section+1))
+# Close the files.
+wr.close()
+#ww.close()
