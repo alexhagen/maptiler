62187b882c21e70df561a990d3495ca75b301b59
Alex Hagen <ahagen@purdue.edu>
2015-03-19 21:18:48 -0400

    cleaned directory

--- mapcrop.m
+++
@@ -1,8 +0,0 @@
-function [f,l,r,t,b]=mapcrop(f)
-    [X,map] = imread(f);
-    I = ind2rgb(X,map);
-    I2 = imcrop(I,[1 1 size(I,1) size(I,2)-50]);
-    [l,r,t,b]=mapedgecheck(I2);
-    f=[f(1:end-4) 'cropped.png'];
-    imwrite(I2,f);
-end
