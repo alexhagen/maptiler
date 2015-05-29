d58326e2847c09d154783951b20933036cd759e4
Alex Hagen <ahagen@mercury.localdomain>
2015-05-29 17:35:24 -0400

    working pretty map!

--- test.py
+++ test.py
@@ -7,14 +7,14 @@ css = open('styles.css').read()
 config = {
 	"proj": {
 		"id": "satellite",
-	    "lon0": -76,
-	    "lat0": 34.50435523955831,
-	    "dist": 1.1,
-	    "up": -32.122944029340516,
-	    "tilt": -3.002581776358639
+	    "lon0": 8.9328,
+	    "lat0": 44.4111,
+	    "dist": 1.08,
+	    "up": -20.0,
+	    "tilt": -2.0
 	},
 	"layers": {	
-		"mygraticule": { "special": "graticule", "latitudes": 3, "longitudes": 3, },	
+		"mygraticule": { "special": "graticule", "latitudes": 3, "longitudes": 3, },
 		"lakes": { 
 			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_lakes.shp",
 			"simplify": 0,
@@ -31,12 +31,24 @@ config = {
 			"src": "/home/ahagen/mapdata/10m_cultural/ne_10m_admin_0_countries.shp",
 			"simplify": 0,
 		},
+		"bath5": { 
+			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_H_3000.shp",
+			"simplify": 0,
+		},
+		"bath4": { 
+			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_I_2000.shp",
+			"simplify": 0,
+		},
+		"bath3": { 
+			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_J_1000.shp",
+			"simplify": 0,
+		},
 		"bath2": { 
-			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_L_0.shp",
+			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_K_200.shp",
 			"simplify": 0,
 		},
 		"bath1": { 
-			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_K_200.shp",
+			"src": "/home/ahagen/mapdata/10m_physical/ne_10m_bathymetry_all/ne_10m_bathymetry_L_0.shp",
 			"simplify": 0,
 		},
 	},
@@ -45,10 +57,26 @@ config = {
 		"width": 800,
 		"round": 2
 	},
-	"bounds": {
+		"bounds": {
 		"mode": "bbox",
-		"data": [-180., -90., -51.0, 41.9]
+		"data": [5.15, 39.35, 16.1667, 46.2],
+		"crop": [2.15, 37.35, 20.1667, 52.2],
 	}
 }
 
 K.generate(config,outfile='/home/ahagen/code/maps/mymap.svg', stylesheet=css);
+
+'''
+		"geneva": {
+			"src": "/home/ahagen/mapdata/10m_cultural",
+			"simplify": 0
+		},
+		"cinque-terra": {
+			"src": "/home/ahagen/mapdata/10m_cultural",
+			"simplify": 0
+		},
+		"gran-paradiso": {
+			"src": "/home/ahagen/mapdata/10m_cultural",
+			"simplify": 0
+		}
+'''
